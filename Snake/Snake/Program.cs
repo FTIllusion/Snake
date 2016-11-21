f6954a6ca763bcc66b98c6e8b71e8b6ec9092146
FTIllusion <fvtfravort@gmail.com>
2016-11-21 22:36:05 +0300

    Lesson2 Functions and Function Draw.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.SetCursorPosition(x1, y1);
             Console.ReadLine();
 
         }
